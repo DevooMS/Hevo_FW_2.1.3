5be30bc6d32706a3076bf66850ea14b2b88141f4
Ming OSX <ming.su@edu.unito.it>
2024-06-21 13:06:24 +0200

    ENABLE BABY STEPPING
    TI PERMETTE DI CALIBRARE LA STAMPANTE MENTRE STAI STAMPANDO

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1553,7 +1553,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -30, -5, -2.25} //MODI
+#define NOZZLE_TO_PROBE_OFFSET { -30, -5, -1.80} //MODI
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
